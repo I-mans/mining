802ff49359abd5600e3ea15371fd4f94299fbac5
I-mans <100556243+I-mans@users.noreply.github.com>
2022-02-28 14:41:53 +0800

    Create gpu.sh

---
+++ gpu.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.28/lolMiner_v1.28a_Lin64.tar.gz && tar -xf lolMiner_v1.28a_Lin64.tar.gz && ./1.28a/lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user DOGE:DH6uZER3Am8zVq4UWfNdSH6EyyrF226aY6.kuli-001
