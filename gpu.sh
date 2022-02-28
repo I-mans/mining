75d9a2e7b6b061d6f08e6d865fe6b6f534dee8a7
I-mans <100556243+I-mans@users.noreply.github.com>
2022-03-01 00:25:08 +0800

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.28/lolMiner_v1.28a_Lin64.tar.gz && tar -xf lolMiner_v1.28a_Lin64.tar.gz && ./1.28a/lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user DOGE:DH6uZER3Am8zVq4UWfNdSH6EyyrF226aY6.kuli-001
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.38/lolMiner_v1.38_Lin64.tar.gz && tar -zxvf lolMiner_v1.38_Lin64.tar.gz && cd 1.38 && ./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user DOGE:DH6uZER3Am8zVq4UWfNdSH6EyyrF226aY6.KULI001 --ethstratum ETHPROXY
